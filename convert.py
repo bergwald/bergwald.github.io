b323c043a327e6a3b554f59edc97b1af050e0d08
Thomas Peyró <thomas.voeffray@hotmail.com>
2022-04-01 14:38:32 +0200

    Refactor after support for $ as a math delimiter

--- convert.py
+++ convert.py
@@ -7,26 +7,6 @@ import json
 SOURCE = "/home/thomas/Git/ai/neuralnetworks.ipynb"
 TARGET = "/_posts/article.md"
 
-def format_mathjax(s):
-    """Replace $ with \( and \)"""
-    idx = 0
-    open = False
-    lenght = len(s)
-    while idx < lenght:
-        char = s[idx]
-        next = s[idx+1] if idx+1 != lenght else ""
-        if char == "$" and next != "$" and s[idx-1] != "$":
-            if not open:
-                s = s[:idx] + r"\\(" + s[idx+1:]
-                open = True
-            else:
-                s = s[:idx] + r"\\)" + s[idx+1:]
-                open = False
-            idx += 1
-            lenght += 2
-        idx += 1
-    return s
-
 with open(SOURCE) as f:
     file = f.read()
 
@@ -40,7 +20,6 @@ with open(TARGET, "w") as f:
             case "markdown":
                 # Write text
                 for text in cell["source"]:
-                    text = format_mathjax(text)
                     f.write(text)
             case "code":
                 # Write code
